1c5ced36bcf41cfb27b76462ac2582ad1c033d57
shresthaoshan <os02411418@student.ku.edu.np>
2020-10-14 00:51:49 +0545

    github actions test verification

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -13,7 +13,8 @@ export default function Index(props) {
 
     return(
         <ShopTemplate>
-            <h3>Hello world!!</h3>
+            <h3>Hello world!!!</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet ipsum blanditiis debitis quia vitae quos, in a neque, dolorem quisquam repellendus explicabo quod veniam. Illum eveniet culpa fugiat ab veniam!</p>
         </ShopTemplate>
     )
 }
